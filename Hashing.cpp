456abc7a8e4d3e73ed5023e998f1e42f9addb550
Satyam TIwari <satyam.25scs1003005312@iilm.edu>
2026-08-19 22:58:02 +0530

    Add files via upload

--- Hashing.cpp
+++ Hashing.cpp
@@ -23,3 +23,50 @@ int main(){
         cout<<num<<"-->"<<hashh[num];
     }
 }
+
+
+
+/***************************************************************************************************
+ **************************************HASHING USING ARRAY******************************************
+ */
+
+
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     int n;
+//     cout<<"Enter size of array : ";
+//     cin>>n;
+//     int arr[n];
+
+//     //we can also use map only.
+//     cout<<"Enter your array : ";
+//     for(int i =0;i<n;i++){
+//         cin>>arr[i];
+        
+//     }
+//     int maxval;
+//     cout<<"Enter maximum value of that array : ";
+//     cin>>maxval;
+//     int hashh[maxval+1]={0};
+//     for(int i =0;i<n;i++){
+//         hashh[arr[i]]++;
+//     }
+
+
+
+//     int q;
+//     cout<<"Enter number of quaries :  ";
+//     cin>>q;
+//     for(int i = 0;i<q;i++){
+//         int num;
+//         cin>>num;
+
+//         if(num<=maxval && num>=0){
+//         cout<<num<<"-->"<<hashh[num];
+//         }
+//         else{
+//         cout<<num<<"-->"<<"0";
+//         }
+//     }
+// }
