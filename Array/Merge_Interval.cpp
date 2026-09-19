ea937b6e199cab1ccab49ead5bbfb08f82832283
Satyam TIwari <satyam.25scs1003005312@iilm.edu>
2026-09-19 17:09:23 +0530

    Add files via upload

---
+++ Array/Merge_Interval.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    // Function to merge overlapping intervals
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        // Sort intervals based on starting time
+        sort(intervals.begin(), intervals.end());
+
+        // Vector to store final merged intervals
+        vector<vector<int>> merged;
+
+        // Traverse each interval
+        for (auto interval : intervals) {
+            // If merged is empty or current interval does not overlap
+            if (merged.empty() || merged.back()[1] < interval[0]) {
+                
+                merged.push_back(interval);
+            } else {
+                merged.back()[1] = max(
+                    merged.back()[1],
+                    interval[1]
+                );
+            }
+        }
+
+        return merged;
+    }
+};
+
+int main() {
+    Solution sol;
+    vector<vector<int>> intervals = {
+        {1, 3}, {2, 6}, {8, 10}, {15, 18}
+    };
+
+    vector<vector<int>> result = sol.merge(intervals);
+
+    for (auto v : result) {
+        cout << "[" << v[0] << "," << v[1] << "] ";
+    }
+
+    return 0;
+}
